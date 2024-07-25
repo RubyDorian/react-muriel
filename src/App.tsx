b61bd2958b6952bfe056682901a1f84a4281f828
Denis Klyuev <denis.klyuev@gmail.com>
2024-07-24 23:49:05 -0400

    load muriel

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import {useRef, useState} from 'react';
 import * as THREE from 'three'
-import { Canvas, useFrame, ThreeElements } from '@react-three/fiber'
+import {Canvas, useFrame, ThreeElements} from '@react-three/fiber'
 
 import './App.css';
+import {useGLTF} from '@react-three/drei';
 
-function Box(props: ThreeElements['mesh']) {
+const Box = (props: ThreeElements['mesh']) => {
     const meshRef = useRef<THREE.Mesh>(null!)
     const [hovered, setHover] = useState(false)
     const [active, setActive] = useState(false)
@@ -19,20 +20,32 @@ function Box(props: ThreeElements['mesh']) {
             onClick={() => setActive(!active)}
             onPointerOver={() => setHover(true)}
             onPointerOut={() => setHover(false)}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+            <boxGeometry args={[1, 1, 1]}/>
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'}/>
         </mesh>
+    )
+};
+
+const Mur = (props: ThreeElements['mesh']) => {
+    const groupRef = useRef<THREE.Group>(null);
+    const { nodes } = useGLTF('Muriel.glb');
+
+    return (
+        <group ref={groupRef} {...props} dispose={null}>
+            <primitive object={nodes.Scene}/>
+        </group>
     )
 }
 
 export const App = () => {
-  return (
-      <Canvas>
-          <ambientLight intensity={Math.PI / 2} />
-          <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI} />
-          <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-          <Box position={[-1.2, 0, 0]} />
-          <Box position={[1.2, 0, 0]} />
-      </Canvas>
-  )
+    return (
+        <Canvas>
+            <ambientLight intensity={Math.PI / 2}/>
+            <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} decay={0} intensity={Math.PI}/>
+            <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI}/>
+            <Box position={[-1.2, 0, 0]}/>
+            <Box position={[1.2, 0, 0]}/>
+            <Mur position={[0, 0, 0]}/>
+        </Canvas>
+    )
 };
